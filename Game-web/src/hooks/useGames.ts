71acfa42cf7ef8cbb1d767f1943c7f04d4d82b71
TicMantas <mantas.tickus1998@gmail.com>
2025-05-04 21:29:30 +0200

    Building a Game Cards for the website

--- Game-web/src/hooks/useGames.ts
+++ Game-web/src/hooks/useGames.ts
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import apiClient from "../Service/api-client";
 import { CanceledError } from "axios";
-
-interface Game {
-  id: number;
-  name: string;
+// from this interface we take everything what we need to make a game card and other feathure
+// its the same properties as in the API website
+export interface Game {
+  id: number; // for game id
+  name: string; // its a game name
+  background_image: string; // this is for the image
 }
 
 interface FetchGamesResponse {
@@ -13,23 +15,23 @@ interface FetchGamesResponse {
 }
 
 const useGames = () => {
-  const controller = new AbortController();
-
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
 
   useEffect(() => {
+    const controller = new AbortController();
     apiClient
-      .get<FetchGamesResponse>("/games",{ signal: controller.signal})
+      .get<FetchGamesResponse>("/games", { signal: controller.signal })
       .then((res) => setGames(res.data.results))
       .catch((err) => {
         if (err instanceof CanceledError) return;
-        setError(err.message)});
+        setError(err.message);
+      });
 
-      return () => controller.abort();
+    return () => controller.abort();
   }, []);
 
   return { games, error };
-};
+}; 
 
 export default useGames;
