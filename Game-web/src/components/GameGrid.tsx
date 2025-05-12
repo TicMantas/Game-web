8733eaaec1beb518b9efc6941a0eeaf55a7ace0b
TicMantas <mantas.tickus1998@gmail.com>
2025-05-12 22:02:32 +0200

    Create a generic data Fetching hook

--- Game-web/src/components/GameGrid.tsx
+++ Game-web/src/components/GameGrid.tsx
@@ -7,7 +7,7 @@ import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
-  const { games, error, isLoading } = useGames(); // this is all the properties we need to use from out custom hook to fetch the data
+  const { data, error, isLoading } = useGames(); // this is all the properties we need to use from out custom hook to fetch the data
   // and get loading skeletons while page is loading before it launches
 
   const skeletons = [1, 2, 3, 4, 5, 6];
@@ -18,7 +18,7 @@ const GameGrid = () => {
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={10}>
         {isLoading &&
           skeletons.map((skeleton) =><GameCardContainer><GameCardSkeleton key={skeleton} /></GameCardContainer>)}
-        {games.map((game) => (
+        {data.map((game) => (
           <GameCardContainer><GameCard key={game.id} game={game} /></GameCardContainer>
         ))}
       </SimpleGrid>
--- Game-web/src/hooks/useGames.ts
+++ Game-web/src/hooks/useGames.ts
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import apiClient from "../Service/api-client";
 import { CanceledError } from "axios";
+import useData from "./useData";
 
 // An inteface Platforms let us see what Platforms can be used for that specific game
 export interface Platforms {
@@ -20,37 +21,9 @@ export interface Game {
   metacritic: number;// to display a critics of each game we fetching
 }
 
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
+
 
 // Custom made hook to take everything we need from the API to disegn the website as we need :D
-const useGames = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false)
-
-  useEffect(() => {
-    const controller = new AbortController();
-    setLoading(true); // Loading is true when we uploading the games and its not showed on the page yet
-    apiClient
-      .get<FetchGamesResponse>("/games", { signal: controller.signal })// get all the games from the API
-      .then((res) => {
-        setGames(res.data.results);// then we get a response from the API with a result
-        setLoading(false); // Loading is set to false when game is uploaded then it will remove loading from the page
-  })
-      .catch((err) => { // and then we catch an error if there is one while we fetching the data if we made a mistake for example
-        // /xgames we will receive an error which means something is wrong while we fetching 
-        if (err instanceof CanceledError) return;
-        setLoading(false); // Loading will switch off when error occurs
-        setError(err.message);
-      });
-
-    return () => controller.abort();
-  }, []);
-
-  return { games, error, isLoading };
-}; 
+const useGames = () => useData<Game>("/games")
 
 export default useGames;
