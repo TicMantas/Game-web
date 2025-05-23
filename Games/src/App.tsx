ef45153c32704e26652cfa27e38c9e6c060ccd44
Mantas Tickus <mantastickus@Mantass-MacBook-Air.local>
2025-05-23 21:58:06 +0200

    Build a responsive layout for the Game-web

--- Games/src/App.tsx
+++ Games/src/App.tsx
@@ -1,10 +1,26 @@
-import { Button } from "@chakra-ui/react"
-
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-return(
-  <Button colorScheme="blue">Hello</Button>
-)
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="red.500">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="coral">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="blue.200">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
