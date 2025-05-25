687caf10cb3c14d57dfe4745760f8a34cf10fd48
TicMantas <mantas.tickus1998@gmail.com>
2025-05-25 17:10:54 +0200

    Crop-image

--- Games/src/components/Cards.tsx
+++ Games/src/components/Cards.tsx
@@ -1,6 +1,7 @@
 import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react"
 import type { Game } from "../hooks/useGames"
 import PlatformIcons from "./PlatformIcons"
+import GetCroppedImage from "../services/image-url"
 
 interface Props{
     game: Game
@@ -13,7 +14,7 @@ const Cards = ({game} : Props) => {
     : 'red';
   return (
     <Card borderRadius={10} overflow="hidden">
-        <Image src={game.background_image}/> 
+        <Image src={GetCroppedImage(game.background_image)}/> 
         <CardBody>
             <Heading fontSize="2xl">{game.name}</Heading>
             <HStack justifyContent='space-between'>
