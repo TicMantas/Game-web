4557d4dd94b9544fefd7cb3fd3e7b69d36a26ffa
TicMantas <mantas.tickus1998@gmail.com>
2025-05-25 16:59:39 +0200

    Metacritics

--- Games/src/components/Cards.tsx
+++ Games/src/components/Cards.tsx
@@ -1,4 +1,4 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react"
+import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react"
 import type { Game } from "../hooks/useGames"
 import PlatformIcons from "./PlatformIcons"
 
@@ -6,12 +6,21 @@ interface Props{
     game: Game
 }
 const Cards = ({game} : Props) => {
+    const CriticColor = game.metacritic > 80
+  ? 'green'
+  : game.metacritic >= 70
+    ? 'orange'
+    : 'red';
   return (
     <Card borderRadius={10} overflow="hidden">
         <Image src={game.background_image}/> 
         <CardBody>
             <Heading fontSize="2xl">{game.name}</Heading>
+            <HStack justifyContent='space-between'>
+                
             <PlatformIcons platforms={game.parent_platforms.map(p => p.platform)}/>
+                <Text fontWeight="bold" bg={CriticColor} padding={1} borderRadius={10}>{game.metacritic}</Text>
+            </HStack>
         </CardBody>
     </Card>
   )
