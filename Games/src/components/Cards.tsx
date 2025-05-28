6d2545269750dd8b66d591c091ec21924b595d7a
TicMantas <mantas.tickus1998@gmail.com>
2025-05-28 22:09:16 +0200

    Cleanup-Cards

--- Games/src/components/Cards.tsx
+++ Games/src/components/Cards.tsx
@@ -1,30 +1,37 @@
-import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react"
-import type { Game } from "../hooks/useGames"
-import PlatformIcons from "./PlatformIcons"
-import GetCroppedImage from "../services/image-url"
+import { Box, Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
+import type { Game } from "../hooks/useGames";
+import PlatformIcons from "./PlatformIcons";
+import GetCroppedImage from "../services/image-url";
 
-interface Props{
-    game: Game
+interface Props {
+  game: Game;
 }
-const Cards = ({game} : Props) => {
-    const CriticColor = game.metacritic > 80
-  ? 'green'
-  : game.metacritic >= 70
-    ? 'orange'
-    : 'red';
+const Cards = ({ game }: Props) => {
+  const CriticColor =
+    game.metacritic > 80 ? "green" : game.metacritic >= 70 ? "orange" : "red";
   return (
     <Card>
-        <Image src={GetCroppedImage(game.background_image)}/> 
-        <CardBody>
-            <Heading fontSize="2xl">{game.name}</Heading>
-            <HStack justifyContent='space-between'>
-                
-            <PlatformIcons platforms={game.parent_platforms.map(p => p.platform)}/>
-                <Text fontWeight="bold" bg={CriticColor} padding={1} borderRadius={10}>{game.metacritic}</Text>
-            </HStack>
-        </CardBody>
+      <Image src={GetCroppedImage(game.background_image)} />
+      <CardBody>
+        <HStack justifyContent="space-between">
+          <Box fontSize="xl">
+            <PlatformIcons
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+          </Box>
+          <Text
+            fontWeight="bold"
+            bg={CriticColor}
+            padding={1}
+            borderRadius={10}
+          >
+            {game.metacritic}
+          </Text>
+        </HStack>
+        <Heading marginTop={3} fontSize="3xl">{game.name}</Heading>
+      </CardBody>
     </Card>
-  )
-}
+  );
+};
 
-export default Cards
+export default Cards;
--- Games/src/components/GameGrid.tsx
+++ Games/src/components/GameGrid.tsx
@@ -16,7 +16,7 @@ const GameGrid = ({gameQuery} : Props) => {
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        spacing={10}
+        spacing={6}
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding={3}
       >
